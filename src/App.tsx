db551af141b4c14f4debad927c34ff9770570f7e
Sejal-08 <sejalsankhyan@gmail.com>
2026-07-12 22:34:43 +0530

    Fix unused React import to fix Vercel build

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { FeeProvider } from './FeeContext';
 import { Layout } from './components/Layout';
 import { DashboardOverview } from './components/DashboardOverview';
